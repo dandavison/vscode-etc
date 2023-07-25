fecd1694b8e7fa3e1e4df17c51b8ca3a0bb6f947
Dan Davison <dandavison7@gmail.com>
2023-07-24 22:20:37 -0400

    copy-github-url

--- src/commands/copy-github-url.ts
+++ src/commands/copy-github-url.ts
@@ -2,12 +2,16 @@ import * as vscode from 'vscode';
 import { makeGithubUrl } from '../lib/git';
 
 export async function copyGithubUrl() {
-  const path = vscode.window.activeTextEditor?.document.uri.path;
-  const line = vscode.window.activeTextEditor?.selection.active.line;
-  if (path !== undefined && line !== undefined) {
+  const editor = vscode.window.activeTextEditor;
+  const path = editor?.document.uri.path;
+  const zeroBasedlineNum = editor?.selection.active.line;
+  if (path !== undefined && zeroBasedlineNum !== undefined) {
+    const oneBasedLineNum = zeroBasedlineNum + 1;
     try {
-      const url = makeGithubUrl(path, line);
-      vscode.env.clipboard.writeText(url).then(() => {
+      const url = makeGithubUrl(path, oneBasedLineNum);
+      const lineText = editor?.document.lineAt(zeroBasedlineNum).text;
+      const link = `[\`${lineText?.trim()}\`](${url})`;
+      vscode.env.clipboard.writeText(link).then(() => {
         let disposable = vscode.window.setStatusBarMessage('Copied GitHub URL');
 
         setTimeout(() => {
@@ -16,7 +20,7 @@ export async function copyGithubUrl() {
       });
     } catch (error) {
       vscode.window.showInformationMessage(
-        `Could not determine GitHub URL for ${path}:${line}: ${error}`
+        `Could not determine GitHub URL for ${path}:${oneBasedLineNum}: ${error}`
       );
     }
   }
