b12e3d93072b81570c9794f0b476bd37e44b559d
Dan Davison <dandavison7@gmail.com>
2023-08-05 11:25:02 -0400

    Fix log channel name

--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,4 @@
 import * as vscode from 'vscode';
 
-const outputChannel = vscode.window.createOutputChannel('Process');
+const outputChannel = vscode.window.createOutputChannel('Etc');
 export const log = outputChannel.appendLine;
