e82e581cb5f2734b25da737bda834f55aac1cb7b
Dan Davison <dandavison7@gmail.com>
2023-12-08 13:09:18 -0500

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as githubUrl from './commands/copy-github-url';
 import { emacsclient } from './commands/emacsclient';
 import { magitStatus } from './commands/magit-status';
+import * as wormhole from './lib/wormhole';
 
 export function activate(context: vscode.ExtensionContext) {
   const catalog: [string, () => Promise<void>][] = [
@@ -15,6 +16,10 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.commands.registerCommand(command, handler)
     );
   }
+  let disposable = vscode.workspace.onDidOpenTextDocument(
+    wormhole.openNonWorkspaceFile
+  );
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
--- src/lib/wormhole.ts
+++ src/lib/wormhole.ts
@@ -1,3 +1,30 @@
 import * as http from 'http';
 import * as vscode from 'vscode';
 import { log } from '../log';
+
+export function openNonWorkspaceFile(document: vscode.TextDocument) {
+  log('openNonWorkspaceFile');
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  const isOutsideWorkspace = workspaceFolders
+    ? workspaceFolders.every((folder) => {
+        return !document.uri.fsPath.startsWith(folder.uri.fsPath);
+      })
+    : true;
+
+  if (isOutsideWorkspace) {
+    const options = {
+      hostname: 'localhost',
+      port: 7117,
+      path: `/${encodeURIComponent(document.uri.fsPath)}`,
+      method: 'GET',
+    };
+
+    const req = http.request(options, () => {});
+
+    req.on('error', (error) => {
+      console.error(error);
+    });
+
+    req.end();
+  }
+}
