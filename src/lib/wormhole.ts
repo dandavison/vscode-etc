525d5288d9a3e07501ad2b57994d63c58602f1da
Dan Davison <dandavison7@gmail.com>
2023-12-08 13:08:31 -0500

    Kill "wormhole links". These are just GitHub links.

--- src/lib/wormhole.ts
+++ src/lib/wormhole.ts
@@ -1,13 +1,3 @@
-import * as git from './git';
-import * as github from './github';
-
-const WORMHOLE_DOMAIN = 'wormhole';
-
-export function makeUrl(path: string, line: number): string {
-  const fileData = git.getGitRepoFile(path);
-  return `http://${WORMHOLE_DOMAIN}/${github.getRepoName(
-    fileData.repo.url
-  )}/blob/${fileData.repo.branch}/${fileData.path}?line=${line}`;
-}
-
-}
+import * as http from 'http';
+import * as vscode from 'vscode';
+import { log } from '../log';
