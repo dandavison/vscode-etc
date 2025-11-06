a23501a7fdc178d64839a021aae90aefef97d605
Dan Davison <dandavison7@gmail.com>
2025-11-06 15:56:27 -0500

    rgi

--- src/commands/ripgrep.ts
+++ src/commands/ripgrep.ts
@@ -1,15 +1,25 @@
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 export async function ripgrep() {
   const editor = vscode.window.activeTextEditor;
   if (!editor) {
     return;
   }
-  const pattern = editor.document.getText(editor.selection);
-  let cmd = `rn ${pattern}`;
-  const terminal =
-    vscode.window.activeTerminal || vscode.window.createTerminal();
-  terminal.show();
-  vscode.commands.executeCommand('workbench.action.toggleMaximizedPanel');
-  terminal.sendText(cmd, Boolean(pattern));
+
+  let pattern = editor.document.getText(editor.selection);
+  if (!pattern) {
+    const wordRange = editor.document.getWordRangeAtPosition(
+      editor.selection.active
+    );
+    pattern = wordRange ? editor.document.getText(wordRange) : '';
+  }
+
+  const cmd = pattern ? `rgi '${pattern}'` : 'rgi';
+  await execAsync(`tmux send-keys C-c`);
+  await execAsync(`tmux send-keys '${cmd}' Enter`);
+  await execAsync('open -a Alacritty');
 }
