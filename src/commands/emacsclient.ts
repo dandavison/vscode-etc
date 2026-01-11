ae70309a9f9250077be09327ebf5f4fc26fe2a6e
Dan Davison <dandavison7@gmail.com>
2026-01-11 09:02:35 -0500

    Remove redundant emacsclient and toggleCursorCpp commands

--- src/commands/emacsclient.ts
+++
@@ -1,7 +0,0 @@
-const { spawn } = require('node:child_process');
-import * as vscode from 'vscode';
-
-export async function emacsclient() {
-  const path = vscode.window.activeTextEditor?.document.uri.path;
-  spawn('emacsclient', ['-n', path]);
-}
