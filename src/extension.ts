2671d8b08f9e28d4589d80f25c7a6a8ffa0a66f4
Dan Davison <dandavison7@gmail.com>
2026-01-11 09:48:17 -0500

    Clean up: delete unused and prefix commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,10 @@
 import * as vscode from 'vscode';
 import * as githubUrl from './commands/copy-github-url';
-import { emacsclient } from './commands/emacsclient';
 import { magitStatus, magitShow } from './commands/magit-status';
 import { gitBlame } from './commands/git-blame';
 import { zoomPane } from './commands/zoom-pane';
 import { showExtensionVersion } from './commands/show-extension-version';
 import { ripgrep } from './commands/ripgrep';
-import { toggleCursorCpp } from './commands/cursor-cpp-toggle';
 import { log } from './log';
 import * as wormhole from './commands/wormhole';
 import { togglePythonTypeCheckingMode } from './commands/toggle-python-type-checking';
@@ -18,13 +16,11 @@ export function activate(context: vscode.ExtensionContext) {
     ['etc.copyGithubUrl', githubUrl.copyGithubUrl],
     ['etc.copyGithubMarkdownUrl', githubUrl.copyGithubMarkdownUrl],
     ['etc.gitBlame', gitBlame],
-    ['etc.emacsclient', emacsclient],
     ['etc.magitStatus', magitStatus],
     ['etc.magitShow', magitShow],
     ['etc.ripgrep', ripgrep],
     ['etc.zoomPane', zoomPane],
     ['etc.openViaWormhole', wormhole.openViaWormhole],
-    ['etc.toggleCursorCpp', toggleCursorCpp],
   ];
   for (const [command, handler] of catalog) {
     context.subscriptions.push(
